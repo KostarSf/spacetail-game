c776865be12d4d063057c85a998a8e63127dd429
Maksim Peskov <kostarsf@gmail.com>
2023-08-19 15:06:37 +0300

    Create simple breakout game

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,111 @@
-import { Engine, Loader } from "excalibur";
-import { Player } from "./player";
-import { Resources } from "./resources";
+import { Actor, CollisionType, Color, Engine, vec } from "excalibur";
 
-class Game extends Engine {
-    constructor() {
-      super({width: 800, height: 600});
-    }
-    initialize() {
-      
-      const player = new Player();
-      this.add(player);
+const game = new Engine({
+  width: 800,
+  height: 600,
+});
 
-      const loader = new Loader([Resources.Sword]);
-      this.start(loader);
+game.start();
+
+const paddle = new Actor({
+  x: 150,
+  y: game.drawHeight - 40,
+  width: 200,
+  height: 20,
+  color: Color.Chartreuse,
+});
+
+paddle.body.collisionType = CollisionType.Fixed;
+
+game.add(paddle);
+
+game.input.pointers.primary.on("move", (ev) => {
+  paddle.pos.x = ev.worldPos.x;
+});
+
+const ball = new Actor({
+  x: 100,
+  y: 300,
+  radius: 10,
+  color: Color.Red,
+});
+
+const ballSpeed = vec(100, 100);
+setTimeout(() => {
+  ball.vel = ballSpeed;
+}, 1000);
+
+ball.body.collisionType = CollisionType.Passive;
+
+game.add(ball);
+
+ball.on("postupdate", () => {
+  if (ball.pos.x < ball.width / 2) {
+    ball.vel.x = ballSpeed.x;
+  }
+
+  if (ball.pos.x + ball.width / 2 > game.drawWidth) {
+    ball.vel.x = ballSpeed.x * -1;
+  }
+
+  if (ball.pos.y < ball.height / 2) {
+    ball.vel.y = ballSpeed.y;
+  }
+});
+
+const padding = 20;
+const xoffset = 65;
+const yoffset = 20;
+const columns = 5;
+const rows = 3;
+
+const brickColor = [Color.Violet, Color.Orange, Color.Yellow];
+
+const brickWidth = game.drawWidth / columns - padding - padding / columns;
+const brickHeight = 30;
+const bricks: Actor[] = [];
+for (let j = 0; j < rows; j++) {
+  for (let i = 0; i < columns; i++) {
+    bricks.push(
+      new Actor({
+        x: xoffset + i * (brickWidth + padding) + padding,
+        y: yoffset + j * (brickHeight + padding) + padding,
+        width: brickWidth,
+        height: brickHeight,
+        color: brickColor[j % brickColor.length],
+      })
+    );
+  }
+}
+
+bricks.forEach((brick) => {
+  brick.body.collisionType = CollisionType.Active;
+  game.add(brick);
+});
+
+let colliding = false;
+ball.on("collisionstart", (ev) => {
+  if (bricks.indexOf(ev.other) > -1) {
+    ev.other.kill();
+  }
+
+  const intersection = ev.contact.mtv.normalize();
+
+  if (!colliding) {
+    colliding = true;
+
+    if (Math.abs(intersection.x) > Math.abs(intersection.y)) {
+      ball.vel.x *= -1;
+    } else {
+      ball.vel.y *= -1;
     }
   }
-  
-  export const game = new Game();
-  game.initialize();
+});
+
+ball.on("collisionend", () => {
+  colliding = false;
+});
+
+ball.on('exitviewport', () => {
+  alert('You lose!')
+})
